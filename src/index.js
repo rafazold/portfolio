6cc52568a9b0e9419597226ef54a43f91a3502cd
Rafa <zoldan.rafael@gmail.com>
2020-04-29 19:41:27 +0300

    sharing meta

--- src/index.js
+++ src/index.js
@@ -2,9 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
+import TagManager from 'react-gtm-module';
+
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+const tagManagerArgs = {
+    gtmId: 'GTM-TXV9CWK',
+}
+
+TagManager.initialize(tagManagerArgs)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
