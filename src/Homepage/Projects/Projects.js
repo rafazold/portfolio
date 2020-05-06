a8c309d209f7e832724ad58ea684d2f0a48a7294
Rafa <zoldan.rafael@gmail.com>
2020-05-06 17:32:42 +0300

    project button click

--- src/Homepage/Projects/Projects.js
+++ src/Homepage/Projects/Projects.js
@@ -1,47 +1,34 @@
 import React, {Component, useState} from 'react';
 import './Projects.scss'
-import Modal from "react-bootstrap/Modal";
-import Header from "../../Header/Header";
 import data from '../../projects.json'
-import { Link, useParams } from "react-router-dom";
-import ProjectPage from "../../ProjectPage/ProjectPage";
+import { Link } from "react-router-dom";
 
 const Projects = () => {
-
-    const [showModal, setShowModal] = useState(false);
-    const [currentProject, setCurrentProject] = useState();
-    const handleShowModal = (e) => {
-        // console.log('AAAA: ', e.target.getAttribute('projectid'));
-        setCurrentProject(e.target.getAttribute('projectid'));
-        setShowModal(true);
-    };
-    const handleCloseModal = () => setShowModal(false);
 
         const projectsList = data.sort;
         return (
             <div className="projects" id="projects">
                 {projectsList.map(project => {
-                    return <article className="project-wrapper" key={Math.random().toString(36).substring(2, 15)} onClick={handleShowModal}>
+                    return <article className="project-wrapper" key={Math.random().toString(36).substring(2, 15)}>
                         <div className="image-wrapper">
                             <Link to={`/projects/${data.projects[project].id}`}>
                                 <img
                                     className="project-image"
                                     src={data.projects[project].image}
                                     alt="project image"
-                                    projectId={data.projects[project].id}
                                 />
                             </Link>
                         </div>
                         <div className="project-footer">
                             <span className="project-name">{data.projects[project].name}</span>
-                            <a /*href={project.url}*/ target="_blank"rel="noopener noreferrer" >
+                            <Link to={`/projects/${data.projects[project].id}`}>
                                 <button className="project-button">Go to project<img
                                     className="button-arrow"
                                     src='assets/right-arrow.svg'
                                     alt="arrow"
                                 />
                                 </button>
-                            </a>
+                            </Link>
                         </div>
                     </article>
                 })}
